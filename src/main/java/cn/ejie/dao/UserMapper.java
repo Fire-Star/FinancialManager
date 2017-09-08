cd999a140ade538b495353a9f4335e0a449e0597
syd <1017987171@qq.com>
2017-09-08 17:54:55 +0800

    添加账户查询功能

--- src/main/java/cn/ejie/dao/UserMapper.java
+++ src/main/java/cn/ejie/dao/UserMapper.java
@@ -1,10 +1,18 @@
 package cn.ejie.dao;
 
 import cn.ejie.po.User;
+import cn.ejie.pocustom.UserCustom;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2017/8/9.
  */
 public interface UserMapper {
     public User findUserByUsername(User user);
+
+    public List<UserCustom> findAll() throws Exception;
+
+    public List<UserCustom> findBySql(String sql) throws Exception;
+
 }
