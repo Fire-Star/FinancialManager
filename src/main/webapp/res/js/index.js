9ad1f67830db8f28fc61ec4b4300c15b2d9048f3
xiejun <939642232@qq.com>
2017-09-27 17:33:49 +0800

    弹窗改为滑动显示

--- src/main/webapp/res/js/index.js
+++ src/main/webapp/res/js/index.js
@@ -12,20 +12,8 @@ $('.level2').click(function(){
 	$(this).addClass('select');
 })
 
-$('.person').click(function(e){
-	$('.person-info').toggle();
-	e.stopPropagation();
-});
-$(document).click(function(e){
-	$('.person-info').hide();
-});
-$('.person-info').click(function(e){
-	e.stopPropagation();
-});
-function addEvent(o, evt, func) {
-	if (o.addEventListener) o.addEventListener(evt, func, false);
-	else if (o.attachEvent) o.attachEvent('on' + evt, func);
-}
-addEvent(document.querySelector('iframe').contentWindow.document,'click', function () {
+$('.person').hover(function(e){
+	$('.person-info').show();
+},function(){
 	$('.person-info').hide();
 });
